ddce2ba7f97c12b085ac463d4304768740f7caa8
411180142h <hankfgh1230@gmail.com>
2025-04-24 11:40:53 +0800

    Add files via upload

--- index.py
+++ index.py
@@ -1,16 +1,28 @@
+import requests
+from bs4 import BeautifulSoup
+
+import firebase_admin
+from firebase_admin import credentials, firestore
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+
 from flask import Flask,render_template,request
-from datetime import datetime
+from datetime import datetime, timezone, timedelta
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-	homepage = "<h1>簡宏宥Python網頁</h1>"
+	homepage = "<h1>簡宏宥Python網頁(時間+8)</h1>"
 	homepage += "<a href=/mis>MIS</a><br>"
 	homepage += "<a href=/today>顯示日期時間</a><br>"
 	homepage += "<a href=/welcome?nick=hongyu>傳送使用者暱稱</a><br>"
 	homepage += "<a href=/account>網頁表單傳值</a><br>"
-	homepage += "<a href=/about>宏宥簡介網頁</a><br>"
+	homepage += "<a href=/account>宏宥簡介網頁</a><br>"
+	homepage += "<br><a href=/read>讀取Firestore資料</a><br>"
+	homepage += "<br><a href=/spider>爬取開眼即將上映電影,存到資料庫</a><br>"
 	return homepage
+
 
 @app.route("/mis")
 def course():
@@ -18,18 +30,19 @@ def course():
 
 @app.route("/today")
 def today():
-	now = datetime.now()
+	tz = timezone(timedelta(hours=+8))
+	now = datetime.now(tz)
 	return render_template("today.html",datetime = str(now))
 
 @app.route("/about")
 def me():
-	now = datetime.now()
 	return render_template("about.html")
 
 @app.route("/welcome", methods=["GET"])
 def welcome():
 	user = request.values.get("nick")
-	return render_template("welcome.html", name=user)
+	w = request.values.get("work")
+	return render_template("welcome.html", name= user, work = w)
 
 
 @app.route("/account", methods=["GET", "POST"])
@@ -42,5 +55,55 @@ def account():
 	else:
 		return render_template("account.html")
 
-if __name__ =="__main__":
+
+@app.route("/read")
+def read():
+	Result = ""
+	db = firestore.client()
+	collection_ref = db.collection("靜宜資管")
+	docs = collection_ref.get()
+	for doc in docs:
+		Result += "文件內容 : {}".format(doc.to_dict()) + "<br>"
+	return Result
+
+@app.route("/spider")
+def read():
+	db = firestore.client()
+
+	url = "http://www.atmovies.com.tw/movie/next/"
+	Data = requests.get(url)
+	Data.encoding = "utf-8"
+
+	sp = BeautifulSoup(Data.text, "html.parser")
+	result=sp.select(".filmListAllX li")
+
+	for item in result:
+		img = item. find("img")
+		#print("片名:", img.get("alt"))
+		#print("海報:", img.get("src"))
+		a = item.find("a")
+		#print("介紹.", "https://www.atmovies.com.tw/" + a.get("href"))
+		#print("編號.", a.get("href")[7:19])
+		div = item.find(class_="runtime")
+		#print("日期.", div.text[5:15])
+
+		if div.text.find("片長:")>0:
+			FilmLen = div.text[21:]
+			print("片長:", div.text[21:])
+		else:
+			FilmLen = "無"
+			#print("目前無片長資訊")
+
+			doc = {
+				"title": img.get("alt"),
+				"hyperlink":"https://www.atmovies.com.tw" +　ａ.get("href"),
+				"picture": img.get("src"),
+				"showDate": div.text[5:15],
+				"showLength": FilmLen
+			}
+
+			doc_ref = db.collection("簡宏宥").document(a.get("href")[7:19])
+			doc_ref.set(doc)
+		return "資料庫更新"
+	if __name__ =="__main__":
 	app.run()
