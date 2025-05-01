3b087254112d66a4a59bda166061c093232c52d0
411180142h <hankfgh1230@gmail.com>
2025-05-01 11:01:26 +0800

    webhook0

--- index.py
+++ index.py
@@ -6,7 +6,7 @@ from firebase_admin import credentials, firestore
 cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
 
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,make_response, jsonify
 from datetime import datetime, timezone, timedelta
 
 app = Flask(__name__)
@@ -20,7 +20,8 @@ def index():
 	homepage += "<a href=/account>網頁表單傳值</a><br>"
 	homepage += "<a href=/account>宏宥簡介網頁</a><br>"
 	homepage += "<br><a href=/read>讀取Firestore資料</a><br>"
-	homepage += "<br><a href=/spider>爬取開眼即將上映電影,存到資料庫</a><br>"
+	homepage += "<h1>08ppt準備(電影含分級)</h1>"
+	homepage += "<br><a href=/movie_rate>讀取開眼即將上映電影(含分級資訊),寫入Firestore</a><br>"
 	return homepage
 
 
@@ -137,6 +138,70 @@ else:
 	return render_template("movie.html")
 
 
+@app.route("/rate")
+def rate():
+    url = "http://www.atmovies.com.tw/movie/next/"
+    Data = requests.get(url)
+    Data.encoding = "utf-8"
+    sp = BeautifulSoup(Data.text, "html.parser")
+    result=sp.select(".filmListAllX li")
+    lastUpdate = sp.find(class_="smaller09").text[5:]
+
+    for x in result:
+        picture = x.find("img").get("src").replace(" ", "")
+        title = x.find("img").get("alt")    
+        movie_id = x.find("div", class_="filmtitle").find("a").get("href").replace("/", "").replace("movie", "")
+        hyperlink = "http://www.atmovies.com.tw" + x.find("a").get("href")
+
+        t = x.find(class_="runtime").text
+        showDate = t[5:15]
+
+        showLength = ""
+        if "片長" in t:
+            t1 = t.find("片長")
+            t2 = t.find("分")
+            showLength = t[t1+3:t2]
+
+        r = x.find(class_="runtime").find("img")
+        rate = ""
+        if r != None:
+             rr = r.get("src").replace("/images/cer_", "").replace(".gif", "")
+            if rr == "G":
+                rate = "普遍級"
+            elif rr == "P":
+                rate = "保護級"
+            elif rr == "F2":
+                rate = "輔12級"
+            elif rr == "F5":
+                rate = "輔15級"
+            else:
+                rate = "限制級"
+
+        doc = {
+            "title": title,
+            "picture": picture,
+            "hyperlink": hyperlink,
+            "showDate": showDate,
+            "showLength": showLength,
+            "rate": rate,
+            "lastUpdate": lastUpdate
+        }
+
+        db = firestore.client()
+        doc_ref = db.collection("電影含分級").document(movie_id)
+        doc_ref.set(doc)
+    return "近期上映電影已爬蟲及存檔完畢，網站最近更新日期為：" + lastUpdate
+
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+
+	return make_response(jsonify({"fulfillmentText": "我是簡宏宥聊天機器人"}))
+
+
+
+
 
 if __name__ == "__main__":
-	app.run()
+    app.run(debug=True)
+
